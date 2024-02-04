906aadba6d1d798ae324be79a451d88f0641ded3
yoonus <yoonustehrani28@gmail.com>
2024-02-04 19:13:29 +0330

    tailwind extended color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'ash-gray': '#AEC3B0',
+        'ghost-white': '#F8F7FF',
+        'antique-white':  '#FFEEDD',
+        apricate: '#FFD8BE',
+        'feld-grau': '#586D67',
+        'earth-yellow': '#DDA15E',
+        'rich-black': '#01161E',
+        'pantone-red': '#EF233C',
+        'charcoal': '#2F3E46',
+        'tigers-eye': '#BC6C25',
+        'hooker-green': '#52796F',
+        'dark-violet': '#483C85',
+        'light-violet': '#8F62EB',
+        'khaki': '#F1CFAB',
+        'cream-powder': '#feebda'
+      },
+      fontFamily: {
+        vazir: 'Vazir'
+      }
+    },
   },
   plugins: [],
 }
